4f3abdf28913afd6e354fb6234d41cbdc9975b26
Takumi Zoza <takumi0207@icloud.com>
2022-08-28 16:19:01 +0900

    変数

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,6 +3,6 @@
     <x-slot name="header">ヘッダー１</x-slot>
     コンポーネントテスト１
 
-    <x-tests.card title="タイトル" content="本文1" />
+    <x-tests.card title="タイトル" content="本文1" :message="$message" />
 </x-tests.app>
 
