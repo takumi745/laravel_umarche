be60769c2486c12a289e1f95c74dc19c12c8e00d
Takumi Zoza <takumi0207@icloud.com>
2022-09-03 17:43:39 +0900

    pagination

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -38,7 +38,7 @@ class OwnersController extends Controller
         // ]);
 
         // dd($e_all, $q_get, $q_first, $c_test);
-        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->paginate(3);
 
 
         return view('admin.owners.index', compact('owners'));
@@ -151,7 +151,7 @@ class OwnersController extends Controller
         return view('admin.expired-owners',
        compact('expiredOwners'));
     }
-    
+
     public function expiredOwnerDestroy($id){
         Owner::onlyTrashed()->findOrFail($id)->forceDelete();
         return redirect()->route('admin.expired-owners.index');
